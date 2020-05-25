97a5e774fe0f5c39b46f7a5c6fe2e8cabc2c8491
Ninesss <623840239@qq.com>
2020-05-25 11:01:52 -0400

    4 command

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,55 @@
 const validator = require('validator')
 const chalk = require('chalk')
 const getNotes = require('./notes')
+const yargs = require('yargs')
 
-const msg = getNotes()
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'add a new note',
+    builder:{
+        title:{
+            describe: 'note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body:{
+            describe: 'note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log('Title: '+ argv.title)
+        console.log('Body: '+ argv.body)
+    }
+})
 
-console.log(msg)
-console.log(validator.isURL('https://ss.io'))
-console.log(chalk.green.inverse.bold('Success!'))
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'remove a note',
+    handler: function(){
+        console.log('removing the note!')
+    }
+})
+
+//create list command
+yargs.command({
+    command: 'list',
+    describe: 'list all the note',
+    handler: function(){
+        console.log('listing all the note!')
+    }
+})
+
+//create read command
+yargs.command({
+    command: 'read',
+    describe: 'read a note',
+    handler: function(){
+        console.log('reading the note!')
+    }
+})
+
+console.log(yargs.argv)
