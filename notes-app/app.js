54af525179bcd3881ff9695609e7e33e88c026d8
Ninesss <623840239@qq.com>
2020-05-25 09:48:52 -0400

    nodemon chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,9 @@
 const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes')
 
 const msg = getNotes()
 
 console.log(msg)
 console.log(validator.isURL('https://ss.io'))
+console.log(chalk.green.inverse.bold('Success!'))
